6e0a30ce451617f3a42bddcf9c65f3cbd017373c
Orthae <orthaefluorite@gmail.com>
2024-11-24 19:31:49 +0100

    missing item test

--- src/domain/item.rs
+++ src/domain/item.rs
@@ -36,3 +36,48 @@ pub enum ItemValidationError {
     #[error("Table id cannot be negative.")]
     NegativeTableId,
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_create_item() {
+        let id = Uuid::new_v4();
+        let table_id = 1;
+        let name = "Pierogi".to_string();
+        let preparation_time = Utc::now();
+
+        let item = Item::try_new(id, table_id, name.clone(), preparation_time)
+            .expect("Failed to create item");
+
+        assert_eq!(item.id, id);
+        assert_eq!(item.table_id, table_id);
+        assert_eq!(item.name, name);
+        assert_eq!(item.preparation_time, preparation_time);
+    }
+    
+    #[test]
+    fn should_not_create_item_with_empty_name() {
+        let id = Uuid::new_v4();
+        let table_id = 1;
+        let name = "".to_string();
+        let preparation_time = Utc::now();
+
+        let result = Item::try_new(id, table_id, name, preparation_time);
+
+        assert_eq!(result, Err(ItemValidationError::EmptyName));
+    }
+    
+    #[test]
+    fn should_not_create_item_with_negative_table_id() {
+        let id = Uuid::new_v4();
+        let table_id = -1;
+        let name = "Pierogi".to_string();
+        let preparation_time = Utc::now();
+
+        let result = Item::try_new(id, table_id, name, preparation_time);
+
+        assert_eq!(result, Err(ItemValidationError::NegativeTableId));
+    }
+}
